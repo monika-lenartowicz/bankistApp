8a14ea223a40f085527884ffeeb80c0644ecd59d
Monika Lenartowicz <nika.nel@interia.pl>
2023-01-23 11:13:52 +0100

    Filter deposits and withdrawals from accounts

--- script.js
+++ script.js
@@ -107,3 +107,10 @@ const createUsernames = accs => {
 
 createUsernames(accounts);
 console.log(accounts);
+
+const deposits = movements.filter(mov => mov > 0);
+
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
