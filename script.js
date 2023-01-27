df421684f587032dd771e8cea7c892dafb9f392c
Monika Lenartowicz <nika.nel@interia.pl>
2023-01-27 14:39:00 +0100

    Added new data

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
-// Data
+///NOTE Data /
+/*
 const account1 = {
 	owner: "Jonas Schmedtmann",
 	movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -35,6 +36,52 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
+
+//NOTE DIFFERENT DATA! Contains movement dates, currency and locale
+*/
+
+const account1 = {
+	owner: "Jonas Schmedtmann",
+	movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+	interestRate: 1.2, // %
+	pin: 1111,
+
+	movementsDates: [
+		"2019-11-18T21:31:17.178Z",
+		"2019-12-23T07:42:02.383Z",
+		"2020-01-28T09:15:04.904Z",
+		"2020-04-01T10:17:24.185Z",
+		"2020-05-08T14:11:59.604Z",
+		"2020-05-27T17:01:17.194Z",
+		"2020-07-11T23:36:17.929Z",
+		"2020-07-12T10:51:36.790Z",
+	],
+	currency: "EUR",
+	locale: "pt-PT", // de-DE
+};
+
+const account2 = {
+	owner: "Jessica Davis",
+	movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+	interestRate: 1.5,
+	pin: 2222,
+
+	movementsDates: [
+		"2019-11-01T13:15:33.035Z",
+		"2019-11-30T09:48:16.867Z",
+		"2019-12-25T06:04:23.907Z",
+		"2020-01-25T14:18:46.235Z",
+		"2020-02-05T16:33:06.386Z",
+		"2020-04-10T14:43:26.374Z",
+		"2020-06-25T18:49:59.371Z",
+		"2020-07-26T12:01:20.894Z",
+	],
+	currency: "USD",
+	locale: "en-US",
+};
+
+const accounts = [account1, account2];
+
 // Elements
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
@@ -61,20 +108,17 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
 const currencies = new Map([
 	["USD", "United States dollar"],
 	["EUR", "Euro"],
 	["GBP", "Pound sterling"],
 ]);
 
+/////////////////////////////////////////////////
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 containerMovements.innerHTML = "";
-console.log(containerMovements.innerHTML);
-/////////////////////////////////////////////////
+
 const displayMovements = function (movements, sort = false) {
 	const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
@@ -233,6 +277,10 @@ btnSort.addEventListener("click", function (e) {
 	sorted = !sorted;
 });
 
+//////////////////////////////////////////////
+///// NOTE -----EXERCISE ARRAY---- NOTE //////
+//////////////////////////////////////////////
+
 //reduce = accumulator → SNOWBALL
 
 //maximum balance__value
@@ -269,8 +317,7 @@ console.log(overallBalance2);
 // const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
 // console.log(overallBalance);
 
-///// NOTE -----EXERCISE ARRAY---- NOTE //////
-
+// EXERCISE ARRAY
 // 1.
 const bankDepositSum = accounts
 	.map(account => account.movements)
